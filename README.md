dfbc28e08071f9ca011593e567e60d931bbbf001
Joseph Tafese <jetafese@uwaterloo.ca>
2023-12-18 21:53:04 -0500

    feat: update comments in file

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # benalohCrypto
+
+This project depends on:
+- [cryptol](https://github.com/GaloisInc/cryptol/tree/master) for (.cry) files
+- [clang](https://github.com/llvm/llvm-project), [gmp](https://gmplib.org/) (for .cpp) files
+
+## co685.cry
+The goal of this program is to capture the mathematical behaviour of the benaloh encryption scheme. It allows us to utilize the higher level constructions in cryptol (a domain specific language) to stay true to the mathematics behind the scheme.
+
+I have added helper functions to achieve a program that is easy to understand. It is useful to validate the scheme in this language since the mathematical constructs can be easy checked (as in the last line) by random testing, exhausing testing and/or formal verification using [Z3](https://github.com/jetafese/z3).
+
+## co685.cpp
+This program implements a prototype benaloh encryption scheme following the description in the following [workshop](https://iopscience.iop.org/article/10.1088/1742-6596/1542/1/012039/pdf). This is imperative code that is compiled and run using the clang compiler.
+
+The assertions can be detected by our software verification [tool](https://github.com/jetafese/seahorn) for formal verification. Since the tool is not built with cryptography in mind, the computation times are quite slow - this is something that can be fixed in the future. 
+
--- co685.cpp
+++ co685.cpp
@@ -3,71 +3,73 @@
 #include <iostream>
 
 void generateRandomNumber(mpz_t n, const mpz_t limit) {
-    gmp_randstate_t state;
-    gmp_randinit_default(state);
-    gmp_randseed_ui(state, static_cast<unsigned long>(time(nullptr)));
+  gmp_randstate_t state;
+  gmp_randinit_default(state);
+  gmp_randseed_ui(state, static_cast<unsigned long>(time(nullptr)));
 
-    mpz_urandomm(n, state, limit);
+  mpz_urandomm(n, state, limit);
 
-    gmp_randclear(state);
+  gmp_randclear(state);
 }
 
-void getBobParameters(mpz_t r, mpz_t y, mpz_t x, const mpz_t p, const mpz_t q, const mpz_t n) {
-    // set r
-    gmp_printf("Finding an appropriate value for r\n");
-    mpz_t limit, pmo, qmo, pmoDiv, gcdB, gcdC;
-    mpz_inits(limit, pmo, qmo, pmoDiv, gcdB, gcdC, NULL);
-    mpz_sub_ui(pmo, p, 1);
-    mpz_sub_ui(qmo, q, 1);
-    mpz_set(limit, q);
-    if (mpz_cmp(p, q) < 0) {
-        mpz_set(limit, p);
+void getBobParameters(mpz_t r, mpz_t y, mpz_t x, const mpz_t p, const mpz_t q,
+                      const mpz_t n) {
+  // set r
+  gmp_printf("Finding an appropriate value for r\n");
+  mpz_t limit, pmo, qmo, pmoDiv, gcdB, gcdC;
+  mpz_inits(limit, pmo, qmo, pmoDiv, gcdB, gcdC, NULL);
+  mpz_sub_ui(pmo, p, 1);
+  mpz_sub_ui(qmo, q, 1);
+  mpz_set(limit, q);
+  if (mpz_cmp(p, q) < 0) {
+    mpz_set(limit, p);
+  }
+  do {
+    generateRandomNumber(r, limit);
+    if (mpz_divisible_p(pmo, r) == 0) {
+      continue;
     }
-    do {
-        // generateRandomNumber(r, limit);
-        if (mpz_divisible_p(pmo, r) == 0) {
-            continue;
-        }
-        mpz_divexact(pmoDiv, pmo, r);
-        mpz_gcd(gcdB, r, pmoDiv);
-        mpz_gcd(gcdC, r, qmo);
-        // gmp_printf("pmo qmo gB bC: %Zd %Zd %Zd %Zd\n", pmoDiv, qmo, gcdB, gcdC);
-    } while ((mpz_divisible_p(pmo, r) == 0) || (mpz_cmp_ui(gcdB, 1) != 0) || (mpz_cmp_ui(gcdC, 1) != 0));
-    gmp_printf("found r: %Zd\n", r);
-    // set y
-    gmp_printf("Finding an appropriate value for x and y\n");
-    mpz_t phi, phiDiv;
-    mpz_inits(phi, phiDiv, NULL);
-    mpz_mul(phi, pmo, qmo);
-    mpz_divexact(phiDiv, phi, r);
-    do {
-        generateRandomNumber(y, n);
-        mpz_powm(x, y, phiDiv, n);
-        gmp_printf("x y phiDiv r: %Zd %Zd %Zd %Zd\n", x, y, phiDiv, r);
-    } while(mpz_cmp_ui(x, 1) == 0);
-    gmp_printf("found x, y: %Zd, %Zd\n", x, y);
-    mpz_clears(phi, phiDiv, NULL);
-    mpz_clears(limit, pmo, qmo, pmoDiv, gcdB, gcdC, NULL);
+    mpz_divexact(pmoDiv, pmo, r);
+    mpz_gcd(gcdB, r, pmoDiv);
+    mpz_gcd(gcdC, r, qmo);
+    // gmp_printf("pmo qmo gB bC: %Zd %Zd %Zd %Zd\n", pmoDiv, qmo, gcdB, gcdC);
+  } while ((mpz_divisible_p(pmo, r) == 0) || (mpz_cmp_ui(gcdB, 1) != 0) ||
+           (mpz_cmp_ui(gcdC, 1) != 0));
+  gmp_printf("found r: %Zd\n", r);
+  // set y
+  gmp_printf("Finding an appropriate value for x and y\n");
+  mpz_t phi, phiDiv;
+  mpz_inits(phi, phiDiv, NULL);
+  mpz_mul(phi, pmo, qmo);
+  mpz_divexact(phiDiv, phi, r);
+  do {
+    generateRandomNumber(y, n);
+    mpz_powm(x, y, phiDiv, n);
+    gmp_printf("x y phiDiv r: %Zd %Zd %Zd %Zd\n", x, y, phiDiv, r);
+  } while (mpz_cmp_ui(x, 1) == 0);
+  gmp_printf("found x, y: %Zd, %Zd\n", x, y);
+  mpz_clears(phi, phiDiv, NULL);
+  mpz_clears(limit, pmo, qmo, pmoDiv, gcdB, gcdC, NULL);
 }
 
 void generateDistinctPrimes(mpz_t prime1, mpz_t prime2, unsigned int bitSize) {
-    gmp_randstate_t state;
-    gmp_randinit_default(state);
-    gmp_randseed_ui(state, static_cast<unsigned long>(time(nullptr)));
+  gmp_randstate_t state;
+  gmp_randinit_default(state);
+  gmp_randseed_ui(state, static_cast<unsigned long>(time(nullptr)));
 
-    // Generate first prime
-    do {
-        mpz_urandomb(prime1, state, bitSize);
-        mpz_nextprime(prime1, prime1);
-    } while (mpz_probab_prime_p(prime1, 25) == 0);
+  // Generate first prime
+  do {
+    mpz_urandomb(prime1, state, bitSize);
+    mpz_nextprime(prime1, prime1);
+  } while (mpz_probab_prime_p(prime1, 25) == 0);
 
-    // Generate second distinct prime
-    do {
-        mpz_urandomb(prime2, state, bitSize);
-        mpz_nextprime(prime2, prime2);
-    } while (mpz_probab_prime_p(prime2, 25) == 0 && mpz_cmp(prime1, prime2) != 0);
+  // Generate second distinct prime
+  do {
+    mpz_urandomb(prime2, state, bitSize);
+    mpz_nextprime(prime2, prime2);
+  } while (mpz_probab_prime_p(prime2, 25) == 0 && mpz_cmp(prime1, prime2) != 0);
 
-    gmp_randclear(state);
+  gmp_randclear(state);
 }
 
 // Benaloh Encryption Function
@@ -86,14 +88,15 @@ void benalohEncrypt(mpz_t result, const mpz_t message, const mpz_t y,
 
 // Benaloh Decryption Function (Brute Force for Small Numbers)
 void benalohDecrypt(mpz_t decrypted, const mpz_t ciphertext, const mpz_t p,
-                    const mpz_t q, const mpz_t r, const mpz_t x, const mpz_t n) {
+                    const mpz_t q, const mpz_t r, const mpz_t x,
+                    const mpz_t n) {
   mpz_t phi, div, a, pmo, qmo;
   mpz_t rth_root;
   mpz_inits(pmo, qmo, a, phi, div, rth_root, NULL);
 
   mpz_sub_ui(pmo, p, 1);
   mpz_sub_ui(qmo, q, 1);
-  gmp_printf("check: %Zd, %Zd, %Zd, %Zd\n", p, pmo, q, qmo);
+  // gmp_printf("check: %Zd, %Zd, %Zd, %Zd\n", p, pmo, q, qmo);
   mpz_mul(phi, pmo, qmo);
   mpz_divexact(div, phi, r);
   mpz_powm(a, ciphertext, div, n);
@@ -129,19 +132,17 @@ int main() {
   // Seed the random state with the current time
   gmp_randseed_ui(state, static_cast<unsigned long>(time(nullptr)));
 
-  // Initialize values
-  mpz_set_ui(p, 397); // p = 11
-  mpz_set_ui(q, 191); // q = 17
-//   generateDistinctPrimes(p, q, 9);
-  mpz_set_ui(r, 99);  // r = 3
-//   mpz_set_ui(x, 24640);
-//   mpz_set_ui(y, 13213);    // y = 5
-//   mpz_set_ui(u, 66183);
+  // Test values
+  // mpz_set_ui(p, 397); // p = 11
+  // mpz_set_ui(q, 191); // q = 17
+  // mpz_set_ui(r, 99);  // r = 3
 
-  mpz_mul(n, p, q); // n = p * q
-  generateRandomNumber(message, r);
+  generateDistinctPrimes(p, q, 32);
+  mpz_mul(n, p, q);
   generateRandomNumber(u, n);
   getBobParameters(r, y, x, p, q, n);
+  generateRandomNumber(message, r);
+  gmp_printf("Original Message: %Zd\n", message);
 
   // Encrypt the message
   benalohEncrypt(encrypted, message, y, r, n, u);
